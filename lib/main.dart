c08e589bfb92cd19cd49596a6387fb3f25d7388f
Pasqat <pasquale.matarrese@gmail.com>
2020-03-31 23:21:40 +0200

    Prova

--- lib/main.dart
+++ lib/main.dart
@@ -7,41 +7,41 @@ void main() {
   ));
 }
 
-class NinaRisponde extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.cyan[200],
-      appBar: AppBar(
-        title: Text('Chiedi a Nina qualsiasi cosa'),
-        backgroundColor: Colors.brown[300],
-      ),
-      body: NinaDice(),
-    );
-  }
-}
-
-class NinaDice extends StatefulWidget {
-  @override
-  _NinaDiceState createState() => _NinaDiceState();
-}
-
-class _NinaDiceState extends State<NinaDice> {
-  int numNina = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        FlatButton(
-            onPressed: () {
-              setState(() {
-                numNina = Random().nextInt(6) + 1;
-              });
-            },
-            child: Image.asset('images/nina$numNina.png')),
-      ],
-    );
-  }
-}
+//class NinaRisponde extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      backgroundColor: Colors.cyan[200],
+//      appBar: AppBar(
+//        title: Text('Chiedi a Nina qualsiasi cosa'),
+//        backgroundColor: Colors.brown[300],
+//      ),
+//      body: NinaDice(),
+//    );
+//  }
+//}
+//
+//class NinaDice extends StatefulWidget {
+//  @override
+//  _NinaDiceState createState() => _NinaDiceState();
+//}
+//
+//class _NinaDiceState extends State<NinaDice> {
+//  int numNina = 1;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Column(
+//      mainAxisAlignment: MainAxisAlignment.end,
+//      children: <Widget>[
+//        FlatButton(
+//            onPressed: () {
+//              setState(() {
+//                numNina = Random().nextInt(6) + 1;
+//              });
+//            },
+//            child: Image.asset('images/nina$numNina.png')),
+//      ],
+//    );
+//  }
+//}
